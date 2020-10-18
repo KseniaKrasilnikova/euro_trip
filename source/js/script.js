c64a3d701bdde228e98199df1cfd660a113da6c7
KseniaKrasilnikova <tksenia13@gmail.com>
2020-10-18 14:07:48 +0300

    html (desktop: tabs)

---
+++ source/js/script.js
@@ -0,0 +1,20 @@
+function openCountry(evt, countryName) {
+    // Объявить все переменные
+    var i, tabcontent, tablinks;
+
+    // Получить все элементы с class="tab-content" и скрыть их
+    tabcontent = document.getElementsByClassName("tab-content");
+    for (i = 0; i < tabcontent.length; i++) {
+        tabcontent[i].style.display = "none";
+    }
+
+    // Получить все элементы с class="tab__links" и удалить class "active"
+    tablinks = document.getElementsByClassName("tab__links");
+    for (i = 0; i < tablinks.length; i++) {
+        tablinks[i].className = tablinks[i].className.replace(" tab__links--active", "");
+    }
+
+    // Показать текущую вкладку и добавить "active" класс для кнопки, открывшей вкладку
+    document.getElementById(countryName).style.display = "block";
+    evt.currentTarget.className += " tab__links--active";
+}
